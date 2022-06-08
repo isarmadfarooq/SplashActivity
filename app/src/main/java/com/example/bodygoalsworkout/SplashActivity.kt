df4e74527992240e20ce3c7473207544b7cf44c4
isarmadfarooq <i.sarmadfarooq@gmail.com>
2022-06-08 23:22:17 +0500

    Initial commit

---
+++ app/src/main/java/com/example/bodygoalsworkout/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bodygoalsworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.splashactivity.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
